f78789990c2887873bb6fd56847ec76a9cae1c8e
Mohamed Thalib <hmthalib@gmail.com>
2013-02-03 23:43:28 +0530

    Minor changes, moded code to function Signed-off-by: Mohamed Thalib <hmthalib@gmail.com>

--- staticblog.py
+++ staticblog.py
@@ -1,9 +1,13 @@
+OUT_DIR = "out"
+IN_FILE = "test.md"
+OUT_FILE = "test.html"
+
 import sys
 sys.path.append(".")
 import markdown2
-IN_FILE = "test.md"
-OUT_FILE = "test.html"
-html = markdown2.markdown_path(IN_FILE)
-fp = open(OUT_FILE, 'w') 
-fp.write(html)
-fp.close()
+
+def convert():
+    html = markdown2.markdown_path(IN_FILE)
+    fp = open(OUT_FILE, 'w')
+    fp.write(html)
+    fp.close()
