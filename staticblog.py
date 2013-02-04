2bee6aa1b2e93fa761a0cf2a8babdf5cc01ac1d6
Mohamed Thalib <hmthalib@gmail.com>
2013-02-04 19:50:51 +0530

    Renamed files, added functions read_file/write_file
    Signed-off-by: Mohamed Thalib <hmthalib@gmail.com>

--- staticblog.py
+++ staticblog.py
@@ -2,20 +2,38 @@ OUT_DIR = "out"
 TEST_DIR = "test"
 IN_FILE = "test_2.md"
 OUT_FILE = "test_2.html"
+HEADER = "site/common/header.ht"
+FOOTER = "site/common/footer.ht"
+
 
 import sys
 import os
 sys.path.append(".")
 import markdown2
 
+def read_file(filename):
+    fp = open(filename, 'r')
+    data = fp.read()
+    fp.close()
+    return data
+
+def write_file(filename, data):
+    fp = open(filename, 'a')
+    fp.write(data)
+    fp.close()
+
+    
 def convert():
     print "Converting"
     in_file = TEST_DIR + "/" + IN_FILE
     out_file = OUT_DIR + "/" + OUT_FILE
     html = markdown2.markdown_path(in_file)
-    fp = open(out_file, 'w')
-    fp.write(html)
-    fp.close()
+    header = read_file(HEADER)
+    footer = read_file(FOOTER)
+    write_file(out_file, header)
+    write_file(out_file, html)
+    write_file(out_file, footer)
+    
 
 def create_dir():
     if not os.path.exists(OUT_DIR):
