fd97349498782b805a6fdebee1a43308469d7e86
Mohamed Thalib <hmthalib@gmail.com>
2013-02-04 12:48:19 +0530

    Read source from /test dir and write it to /out dir
    Signed-off-by: Mohamed Thalib <hmthalib@gmail.com>

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
 staticblog
 ==========
 
-A blog engine to generate static html blog
+A blog engine to generate static HTML blog
+
+This project is using markdown2 python library from https://github.com/trentm/python-markdown2 to generate HTML files from markdown syntax.
+
+This project is at very beginning stage. I have to work a lot on this.
--- staticblog.py
+++ staticblog.py
@@ -1,13 +1,26 @@
 OUT_DIR = "out"
-IN_FILE = "test.md"
-OUT_FILE = "test.html"
+TEST_DIR = "test"
+IN_FILE = "test_2.md"
+OUT_FILE = "test_2.html"
 
 import sys
+import os
 sys.path.append(".")
 import markdown2
 
 def convert():
-    html = markdown2.markdown_path(IN_FILE)
-    fp = open(OUT_FILE, 'w')
+    print "Converting"
+    in_file = TEST_DIR + "/" + IN_FILE
+    out_file = OUT_DIR + "/" + OUT_FILE
+    html = markdown2.markdown_path(in_file)
+    fp = open(out_file, 'w')
     fp.write(html)
     fp.close()
+
+def create_dir():
+    if not os.path.exists(OUT_DIR):
+        os.makedirs (OUT_DIR);
+    
+
+create_dir();
+convert();
